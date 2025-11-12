6c813e57cadf5c175f9f9b694f93ba2938385708
RadaHidos <120414438+RadaHidos@users.noreply.github.com>
2025-11-13 00:10:44 +0100

    DESIGN :art: Created the site-layout and added it to Welcome, Index and Articles pages

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,33 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-</head>
-
-
-<body class="white">
-
-    <header class='bg-teal-300 mb-2'>
-        <div class="mx-auto w-2/3">
-            menu
-        </div>
-
-    </header>
-
-    <main>
-        Testing show for {{$article}}
-    </main>
-
-    <footer class="bg-teal-100 mt-10 h-20">
-        <div class="mx-auto w-2/3">
-            Some really important legal stuff
-        </div>
-
-    </footer>
-
-</body>
-
-</html>
+<x-site-layout>
+    Testing show for {{$article}}
+</x-site-layout>
