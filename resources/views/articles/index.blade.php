a294f4ad201168735f9560e02909f6f4932e7314
RadaHidos <120414438+RadaHidos@users.noreply.github.com>
2025-11-12 23:30:59 +0100

    DESIGN :art: changes to the index page layout (added style to header, main, footer)

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,10 +1,32 @@
 <!DOCTYPE html>
 <html>
 
-<body>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
 
-    <h1>My First Heading</h1>
-    <p>My first paragraph.</p>
+
+<body class="white">
+
+    <header class='bg-pink-200 mb-2'>
+        <div class="mx-auto w-2/3">
+            menu
+        </div>
+
+    </header>
+
+    <main>
+        List of articles
+    </main>
+
+    <footer class="bg-pink-100 mt-10 h-20">
+        <div class="mx-auto w-2/3">
+            Some really important legal stuff
+        </div>
+
+    </footer>
 
 </body>
 
