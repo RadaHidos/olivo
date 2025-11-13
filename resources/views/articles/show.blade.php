8106c91268f19c67692dc6ae69d8ed504db3251b
RadaHidos <120414438+RadaHidos@users.noreply.github.com>
2025-11-13 23:44:05 +0100

    DESIGN :art: show the author on the OneArticle page

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,13 +1,24 @@
 <x-site-layout title="{{$article->title}}">
+
+    <div class="font-bold mb-2 text-teal-500">Written by {{$article->author->name}}</div>
+
     {{$article->content}}
 
 
 
     <h2 class="font-bold mb-2 mt-8">Comments</h2>
+
+    @if($article->comments->isNotEmpty())
+
     <ul class="list-disc pl-4">
         @foreach($article->comments as $comment)
         <li>{{$comment->content}}</li>
         @endforeach
     </ul>
+
+    @else
+    <div class="text-gray-500"> No comments yet</div>
+
+    @endif
 
 </x-site-layout>
